0c1c9ceae3e4a660aa425ed8316ec7186ffae597
AkakoDesu <expdesu@gmail.com>
2025-01-01 21:30:50 +0900

    Update JujutsuInfinite.lua

--- JujutsuInfinite.lua
+++ JujutsuInfinite.lua
@@ -992,7 +992,7 @@ SaveManager:BuildConfigSection(Tabs.Settings)
 Window:SelectTab(1)
 
 Fluent:Notify({
-    Title = "Fluent",
+    Title = "Tokyo Hub",
     Content = "The script has been loaded.",
     Duration = 8
 })
